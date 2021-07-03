5793f057cd2d64536c2675289afac382656c5d4c
Aditi3 <aditi.gvc@gmail.com>
2021-07-03 19:36:03 +0530

    style: Update to Status Bar and Navigation Bar

--- MovieTracker/MovieTracker/ViewController.swift
+++ MovieTracker/MovieTracker/ViewController.swift
@@ -12,12 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         setupUI()
     }
     
     func setupUI() {
         self.title = "Your List"
+        if let navigationBar = self.navigationController?.navigationBar  {
+            navigationBar.barStyle = UIBarStyle.default;
+            navigationBar.barTintColor = UIColor(red: 0.11, green: 0.73, blue: 0.33, alpha: 1.00)
+            navigationBar.backgroundColor = UIColor(red: 0.11, green: 0.73, blue: 0.33, alpha: 1.00)
+            navigationBar.isTranslucent = false
+        }
     }
-    
 }
+
+
+
 
