07c5790c5c3ab6cae35e466cce5c14328f8532eb
Aditi3 <aditi.gvc@gmail.com>
2021-07-04 12:29:45 +0530

    Implement `Equatable` operator

--- MovieTracker/MovieTracker/Models/MovieModel.swift
+++ MovieTracker/MovieTracker/Models/MovieModel.swift
@@ -7,9 +7,19 @@
 
 import Foundation
 
-struct MovieModel {
+struct MovieModel: Equatable {
     
     var title: String
     var releaseDate: String?
     
+}
+
+func==(lhs: MovieModel, rhs: MovieModel) -> Bool {
+    if (lhs.title == rhs.title) {
+        return true
+    }
+    if (lhs.releaseDate == rhs.releaseDate) {
+        return true
+    }
+    return false
 }
--- MovieTracker/MovieTrackerTests/Test Models/MovieStructTests.swift
+++ MovieTracker/MovieTrackerTests/Test Models/MovieStructTests.swift
@@ -9,7 +9,10 @@ import XCTest
 @testable import MovieTracker
 
 class MovieStructTests: XCTestCase {
-
+    
+    let adventureMovie = MovieModel(title: "Adventure")
+    let actionMovie = MovieModel(title: "Action")
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,16 +22,37 @@ class MovieStructTests: XCTestCase {
         super.tearDown()
     }
     
-    func testInit_MovieWithTitle() {
+    func testInit_SetMovieWithTitle() {
         let testMovie = MovieModel(title: "Generic")
         XCTAssertNotNil(testMovie)
         XCTAssertEqual(testMovie.title, "Generic")
     }
     
-    func testInit_MovieTitleAndReleaseDate() {
+    func testInit_SetMovieTitleAndReleaseDate() {
         let testMovie = MovieModel(title: "Comedy", releaseDate: "1991")
         XCTAssertNotNil(testMovie)
         XCTAssertEqual(testMovie.releaseDate, "1991")
+    }
+    
+    func testEquatable_ReturnsTrue() {
+        let actionMovie1 = MovieModel(title: "Action")
+        let actionMovie2 = MovieModel(title: "Action")
+
+        XCTAssertEqual(actionMovie1, actionMovie2)
+    }
+    
+    func testEquatable_ReturnsNotEqualTitles() {
+        let actionMovie1 = MovieModel(title: "Action")
+        let actionMovie2 = MovieModel(title: "Adventure")
+
+        XCTAssertNotEqual(actionMovie1, actionMovie2)
+    }
+    
+    func testEquatable_ReturnsNotEqualReleaseDates() {
+        let actionMovie1 = MovieModel(title: "Action", releaseDate: "1989")
+        let actionMovie2 = MovieModel(title: "Adventure", releaseDate: "2018")
+
+        XCTAssertNotEqual(actionMovie1, actionMovie2)
     }
 
 }
